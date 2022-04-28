233e50cb6f72244e36a24155b899af96854318a5
Natasha <njohnson533@gmail.com>
2022-04-28 16:21:18 -0400

    added Header component

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,9 @@
-const Layout = ({children}) => {
+import Header from "./Header";
+
+const Layout = ({ children }) => {
     return ( 
         <>
+            <Header />
             <main>
                 {children}
             </main>
