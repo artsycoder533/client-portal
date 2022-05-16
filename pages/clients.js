bab8f0092fb445b069611aae372e6acd801ff4f8
Natasha <njohnson533@gmail.com>
2022-05-16 17:41:11 -0400

    added useEffect to pull client data from firebase

--- pages/clients.js
+++ pages/clients.js
@@ -1,14 +1,28 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from "../Context";
 import { useRouter } from 'next/router';
+import { collection, onSnapshot } from 'firebase/firestore';
 
 const Clients = () => {
+    const [clients, setClients] = useState([]);
     const router = useRouter();
     const { user, isLoggedIn } = useContext(AuthContext);
    
     useEffect(() => {
         isLoggedIn ? router.push("clients") : router.push("/login");
     }, [isLoggedIn]);
+
+    //get clients from firebase
+    useEffect(() => {
+        const clientCollection = collection(db, "clients");
+        const q = query(clientCollection, orderBy("lastName", "desc"));
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            const snapshotDocs = [];
+            snapshot.forEach((doc) => snapshotDocs.push(doc.data()));
+            setClients(snapshotDocs);
+        });
+        return unsubscribe;
+    }, []);
 
     return (
         <section>
