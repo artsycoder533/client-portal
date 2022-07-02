db9490f69d2939f5a43884b749661935c09cc7a9
Natasha <njohnson533@gmail.com>
2022-07-02 18:16:39 -0400

    wip

--- pages/clients/[id].js
+++ pages/clients/[id].js
@@ -1,8 +1,54 @@
-const Client = (props) => {
-    console.log(props);
-    return ( 
-        <h2>This is the client page</h2>
-     );
+import { collection, getDocs, getDoc } from "firebase/firestore";
+import { db} from "../../utils/firebase"
+import { getAuth } from "firebase/auth";
+
+const Client = ({ clientProps }) => {
+    const clients = JSON.parse(clientProps)
+   console.log(clientProps)
+  return (
+    <>
+      <h2>This is the client page</h2>
+      <p>{clients.firstName}</p>
+    </>
+  );
 }
  
 export default Client;
+
+export const getStaticPaths = async () => {
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const snapshot = await getDocs(collection(db, user.email));
+  const paths = snapshot.docs.map((doc) => {
+    return {
+      params: { id: doc.id.toString() },
+    };
+  });
+
+  console.log(paths);
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  const id = context.params.id;
+
+  const docRef = doc(db, user.email, id);
+
+  const docSnap = await getDoc(docRef);
+
+  return {
+    props: {
+      clientProps: JSON.stringify(docSnap.data()) || null,
+    },
+  };
+};
+
+
+
